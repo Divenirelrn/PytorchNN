871de8109266ed44c1d349c43bd51eb77274609a
zhaolu <zhaolu16120284@163.com>
2021-01-09 13:31:47 +0800

    修改gpu模块

--- gpu.py
+++ gpu.py
@@ -1,10 +1,32 @@
 import torch
-import os
+import os, time
 
+#############GPU与CPU对比###############
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-#并行计算
-torch.nn.DataParallel()
+#cpu
+a = torch.randn(1, 13 * 13 * 3, 85)
+b = torch.randn(1, 13 * 13 * 3, 85)
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,2,3"
+#gpu
+a = a.to(device)
+b = b.to(device)
+
+s1 = time.time()
+c = a * b
+print("Run time: %f" % (time.time() - s1))
+
+#############多GPU设置###############
+#方法一
+CUDA_VISIBLE_DEVICES = 1,2,3
+
+#方法一
+os.environ["CUDA_VISIBLE_DEVICES"] = "1,2,3"
+
+#方法三
+torch.cuda.set_device(1,2,3)
+
+##############并行计算设置###############
+model = torch.nn.DataParallel(model, device_ids=[1,2,3])
+
 
